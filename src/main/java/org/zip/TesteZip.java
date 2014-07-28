406971cf5f932077ce7c44ac6a96e87b46bcd94f
Renato Batista <sonomel00@yahoo.com.br>
2014-07-28 01:59:51 -0300

    Teste em método main (sem jUnit) de verificação da extração e criação de zip.

--- src/main/java/org/zip/TesteZip.java
+++ src/main/java/org/zip/TesteZip.java
@@ -1,5 +1,6 @@
 package org.zip;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Enumeration;
@@ -8,19 +9,31 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 public class TesteZip {
+	private static final String PASTA_EXTRACAO = "f://zip-teste/testeDeExtracao";
+	private static final String PASTA_COMPACTAR = "f://Testes JAVA/";
+	private static final String ARQUIVO_ZIP_LEITURA = "f://zip-teste/teste.zip";
+	private static final String ARQUIVO_ZIP_ESCRITA = "f://zip-teste/meu_compactado.zip";
+
 	public static void main(String[] args) throws IOException {
 		testeLeituraZipComZipFile();
 		testeLeituraZipComZipInputStream();
 		
 		UnZipper uz = new UnZipper();
 		
-		uz.extrair("c://zip/teste.zip", "c://zip/testeDeExtracao");
+		System.out.println(uz.extrair(ARQUIVO_ZIP_LEITURA, PASTA_EXTRACAO));
+		
+		Zipper z = new Zipper();
+		
+		File zipSaida = new File(ARQUIVO_ZIP_ESCRITA);
+		File conteudoASerCompactado = new File(PASTA_COMPACTAR);
+		
+		z.criarZip(zipSaida, new File[]{conteudoASerCompactado});
 	}
 
 	private static void testeLeituraZipComZipInputStream() throws IOException {
 		System.out.println("Teste de leitura zip com ZipInputStream");
 		
-		String arquivo = "c://zip/teste.zip";
+		String arquivo = ARQUIVO_ZIP_LEITURA;
 		
 		FileInputStream fis = new FileInputStream(arquivo);
 		ZipInputStream zis = new ZipInputStream(fis);
@@ -38,7 +51,7 @@ public class TesteZip {
 	private static void testeLeituraZipComZipFile() throws IOException {
 		System.out.println("Teste de leitura zip com ZipFile");
 		
-		String arquivo = "c://zip/teste.zip";
+		String arquivo = ARQUIVO_ZIP_LEITURA;
 		
 		ZipFile zip = new ZipFile(arquivo);		
 		Enumeration<? extends ZipEntry> e = zip.entries();
